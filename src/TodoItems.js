f6465a332883578e98daeeb0ec17e6442edfcf23
takumab <takumab@gmail.com>
2018-12-27 22:34:24 +0000

    Change color - Add span to completed or not

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -7,7 +7,8 @@ function TodoItems(props) {
                 checked={props.item.completed}
                 onChange={() => props.handleChange(props.item.id)}
             />
-            <span>{props.item.text}</span>
+            <span>{props.item.text} </span>
+            <span style={{color: '#FD5A69'}}>{props.item.completed ? 'Completed' : 'Not yet'}</span>
             <br/>
         </div>
     )
